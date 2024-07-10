ff572f2aeab617de8a50b8bb791e805271b7bd88
Kacper <barabasz64@gmail.com>
2024-07-10 21:12:13 +0200

    Add CSS reset, change layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,17 @@ import ToDoPanel from './components/ToDoPanel';
 function App() {
   return (
     <main className='h-dvh flex flex-col bg-slate-900'>
-      <img src='../public/images/mountain-background.jpg' alt='Image of mountains' className='h-2/6 object-cover' />
-      {/* <ToDoPanel></ToDoPanel> */}
+      <img
+        src='../public/images/mountain-background.jpg'
+        alt='Image of mountains'
+        className='relative h-2/6 object-cover after:absolute after:inset-0 after:bg-black after:z-10'
+      />
+
+      <ToDoPanel></ToDoPanel>
 
       <footer className='mt-auto'>
         <p className='text-zinc-300 font-bold text-center p-4'>
-          Created by <a href='https://github.com/Borub-ar'>Kacper Barabasz</a>
+          Created by <a href='https://github.com/Borub-ar' className='text-emerald-400'>Kacper Barabasz</a>
         </p>
       </footer>
     </main>
--- client/src/components/ToDoPanel.tsx
+++ client/src/components/ToDoPanel.tsx
@@ -1,13 +1,17 @@
 const ToDoPanel = () => {
   return (
-    <div className='bg-white rounded-md w-4/5'>
-      <div className='bg-clr-primary rounded-l-md max-w-80 p-8'>
-        <h1 className='text-2xl font-bold text-white'>ToDo</h1>
+    <div className='relative -top-10 self-center bg-slate-800 rounded-xl w-4/5 h-full'>
+      <h1 className='absolute -top-12 text-4xl font-bold text-white'>ToDo</h1>
 
-        <div></div>
+      <div className='bg-clr-primary rounded-l-xl max-w-80 p-6 h-full'>
+        <p className="text-white text-xl font-bold">Incoming Tasks</p>
+
+        
       </div>
 
-      <div></div>
+      <div>
+
+      </div>
     </div>
   );
 };
