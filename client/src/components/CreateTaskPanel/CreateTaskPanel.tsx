c1f125984d175a0ba33693e1c5fbf1333a94a26d
Kacper <barabasz64@gmail.com>
2024-08-08 19:21:24 +0200

    Fix adding new task

--- client/src/components/CreateTaskPanel/CreateTaskPanel.tsx
+++ client/src/components/CreateTaskPanel/CreateTaskPanel.tsx
@@ -9,11 +9,13 @@ type CreateTaskPanelProps = {
 const CreateTaskPanel = ({ pickedDate }: CreateTaskPanelProps) => {
   const { createNewTask } = useTask();
 
-  const handleFormSubmit = event => {
+  const handleFormSubmit = (event: any) => {
     event.preventDefault();
 
+    if (pickedDate === null) return;
+
     createNewTask({
-      name: event.target.name.value,
+      title: event.target.title.value,
       description: event.target.description.value,
       date: pickedDate,
     });
@@ -26,7 +28,7 @@ const CreateTaskPanel = ({ pickedDate }: CreateTaskPanelProps) => {
       className='bg-slate-600 h-full rounded-md p-4 w-1/2 flex flex-col gap-4 border-2 border-cyan-50 border-solid'>
       <p className='font-bold text-2xl text-white'>Create New Task</p>
 
-      <TextInput title='Task name' id='name' />
+      <TextInput title='Task title' id='title' />
       <TextInput title='Description' id='description' />
 
       <button className='bg-white rounded-md p-3 font-bold mt-auto'>Create New Task</button>
--- server/src/routes/tasks/tasks.controller.js
+++ server/src/routes/tasks/tasks.controller.js
@@ -6,15 +6,15 @@ async function httpGetAllTasks(req, res) {
 }
 
 async function httpAddNewTask(req, res) {
-    const newTask = {
-      title: req.body.title,
-      description: req.body.description,
-      date: req.body.date,
-      taskId: req.body.taskId,
-    };
+  const newTask = {
+    title: req.body.title,
+    description: req.body.description,
+    date: req.body.date,
+    taskId: '121312das231',
+  };
 
   await createNewTask(newTask);
-  return res.status(200).json({ text });
+  return res.status(200).json({ message: 'Task added successfully' });
 }
 
 module.exports = {
